d9e6660d54789d06628315fbe70a8f0c07e6d27a
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-04-21 21:54:57 +0200

    Implement form validation

--- src/app/admin/user-edit/user-edit.component.ts
+++ src/app/admin/user-edit/user-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import {User} from '../admin-interfaces/user';
 import {UsersService} from '../admin-services/users.service';
+import {RxwebValidators} from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-user-edit',
@@ -9,6 +10,7 @@ import {UsersService} from '../admin-services/users.service';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  submitted = false;
   userData;
 
   constructor(private formBuilder: FormBuilder,
@@ -16,15 +18,33 @@ export class UserEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.userData = this.formBuilder.group({
-      firstName: ['', Validators.minLength(5)],
-      lastName: [''],
-      userName: [''],
+      firstName: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      lastName: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      userName: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
       phoneNumber: [''],
-      email: [''],
-      newPassword: [''],
-      repeatPassword: [''],
+      email: ['', [
+        Validators.required,
+        RxwebValidators.email()
+      ]],
+      newPassword: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]],
+      repeatPassword: ['', [
+        Validators.required,
+        RxwebValidators.compare({fieldName: 'newPassword'})
+      ]],
       debt: [''],
-      fundId: [''],
+      fundId: ['', Validators.required],
       info: ['']
     });
   }
@@ -33,7 +53,36 @@ export class UserEditComponent implements OnInit {
     return this.userData.get('firstName');
   }
 
+  get field(): any {
+    return this.userData.controls;
+  }
+
+  get email(): any {
+    return this.userData.get('email');
+  }
+
+  get repeatPassword(): any {
+    return this.userData.get('repeatPassword');
+  }
+
   onSubmit(): void {
-    this.usersService.CreateUser(this.userData.getRawValue());
+    console.log(this.userData.controls);
+    if (this.userData.invalid) {
+      this.submitted = true;
+
+      Object.keys(this.userData.controls).forEach(field => {
+        const control = this.userData.get(field);
+        control.markAsTouched({onlySelf: true});
+      });
+
+      if (this.field.fundId.value.startsWith('Choose')) {
+        this.field.fundId.setErrors({required: true});
+      }
+
+    } else {
+      this.submitted = false;
+
+      this.usersService.CreateUser(this.userData.getRawValue());
+    }
   }
 }
