378db41cd3f98b695319802451b609f70b493694
Sławomir Brzozowski <brzozasr@interia.eu>
2021-04-25 22:21:23 +0200

    Added ReportOrderBySupplierComponent.

---
+++ src/app/reports/report-order-by-supplier/report-order-by-supplier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-order-by-supplier',
+  templateUrl: './report-order-by-supplier.component.html',
+  styleUrls: ['./report-order-by-supplier.component.css']
+})
+export class ReportOrderBySupplierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
