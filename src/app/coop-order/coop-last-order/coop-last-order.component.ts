b89c696b818a4ddb20d8836b60ad751123cf8030
Sławomir Brzozowski <brzozasr@interia.eu>
2021-04-30 22:39:39 +0200

    Implementation of the last order grande started.

---
+++ src/app/coop-order/coop-last-order/coop-last-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coop-last-order',
+  templateUrl: './coop-last-order.component.html',
+  styleUrls: ['./coop-last-order.component.css']
+})
+export class CoopLastOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/coop-order/coop-order.module.ts
+++ src/app/coop-order/coop-order.module.ts
@@ -5,11 +5,13 @@ import { CoopOrderRoutingModule } from './coop-order-routing.module';
 import { CoopOrdersComponent } from './coop-orders/coop-orders.component';
 import {MaterialModule} from '../material/material.module';
 import {NgReplacePipeModule} from 'angular-pipes';
+import { CoopLastOrderComponent } from './coop-last-order/coop-last-order.component';
 
 
 @NgModule({
   declarations: [
-    CoopOrdersComponent
+    CoopOrdersComponent,
+    CoopLastOrderComponent
   ],
   imports: [
     CommonModule,
