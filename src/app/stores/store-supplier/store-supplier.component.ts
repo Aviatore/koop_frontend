44025ad6dd8b461bb56c9c348dcc880896664cf8
Sławomir Brzozowski <brzozasr@interia.eu>
2021-05-03 16:49:27 +0200

    The supplier store partially implemented v1.

--- src/app/stores/store-supplier/store-supplier.component.ts
+++ src/app/stores/store-supplier/store-supplier.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {StoreService} from '../service/store.service';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {SupplierList} from '../models/supplier-list';
 import {filter, map, startWith, switchMap} from 'rxjs/operators';
+import {Info} from '../models/info';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {SupplierProductsList} from '../models/supplier-products-list';
+import {SupplierStore} from '../models/supplier-store';
 
 @Component({
   selector: 'app-store-supplier',
@@ -16,6 +22,16 @@ export class StoreSupplierComponent implements OnInit {
 
   control = new FormControl();
   filteredSuppliers: Observable<SupplierList[]>;
+
+  info: Info;
+  problem: string;
+  supplierStore: SupplierStore;
+  productsList: SupplierProductsList[];
+  dataSource: MatTableDataSource<SupplierProductsList>;
+  itemsPerPage = [10, 25, 50, 100];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private service: StoreService) { }
 
@@ -42,5 +58,29 @@ export class StoreSupplierComponent implements OnInit {
   }
 
   getSupplierProducts(supplierId: string): void {
+    this.suppId = supplierId;
+    this.service.getSupplierStore(supplierId)
+      .subscribe((data) => {
+          if ('info' in data) {
+            this.info = data;
+            this.supplierStore = undefined;
+            this.problem = undefined;
+          } else {
+            this.info = undefined;
+            this.supplierStore = data;
+            this.problem = undefined;
+
+            this.dataSource = new MatTableDataSource(data.supplierProductsList);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+
+            return this.supplierStore;
+          }
+        },
+        err => {
+          this.info = undefined;
+          this.supplierStore = undefined;
+          this.problem = err.error.detail;
+        });
   }
 }
