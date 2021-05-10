b533cb5f9bcfdbe536e6051c032abc9c52e56ccf
Sławomir Brzozowski <brzozasr@interia.eu>
2021-05-11 01:23:11 +0200

    Minor bugfix.

--- src/app/basket-view/basket-view.module.ts
+++ src/app/basket-view/basket-view.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BasketViewRoutingModule } from './basket-view-routing.module';
-import {BacketViewComponent} from './backet-view/backet-view.component';
+import {BasketViewComponent} from './basket-view/basket-view.component';
 import { BasketIconComponent } from './basket-icon/basket-icon.component';
 import {MaterialModule} from '../material/material.module';
 
 
 @NgModule({
   declarations: [
-    BacketViewComponent,
+    BasketViewComponent,
     BasketIconComponent,
   ],
   exports: [
