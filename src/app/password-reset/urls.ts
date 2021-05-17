93ca656a7832e0ac5d2072dfedda58d35ac317b7
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-05-17 13:54:14 +0200

    Add a panel to reset password by logged-in users

--- src/app/password-reset/urls.ts
+++ src/app/password-reset/urls.ts
@@ -1,4 +1,5 @@
 export const PasswordResetUrls = {
   ResetPassword: 'http://localhost:5000/api/auth/reset-password',
-  SetNewPassword: 'http://localhost:5000/api/auth/reset-password-set'
+  SetNewPassword: 'http://localhost:5000/api/auth/reset-password-set',
+  SelfSetNewPassword: 'http://localhost:5000/api/auth/self-reset-password-set',
 };
--- src/app/user-panel/user-panel.module.ts
+++ src/app/user-panel/user-panel.module.ts
@@ -4,16 +4,23 @@ import { CommonModule } from '@angular/common';
 import { UserPanelRoutingModule } from './user-panel-routing.module';
 import { UserComponent } from './user/user.component';
 import {AdminModule} from '../admin/admin.module';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import {RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    PasswordResetComponent
   ],
-  imports: [
-    CommonModule,
-    AdminModule,
-    UserPanelRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        AdminModule,
+        RxReactiveFormsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        UserPanelRoutingModule
+    ]
 })
 export class UserPanelModule { }
